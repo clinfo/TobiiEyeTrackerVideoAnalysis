20a9c9fc1559ccde9d97b4f89def1816190c8d50
MariOkamoto <okamoto.mari.62j@gmail.com>
2021-10-24 23:33:07 +0900

    add new files

---
+++ project/src/scene.py
@@ -0,0 +1,6 @@
+def makeScene():
+    #start = [The first frame of scene x]
+    scene = ['1', '2', '3', '4', '5', '6', '7']
+    start = [0, 243, 448, 771, 951, 1132, 1235, 1562]
+    #df=df.iloc[int((start[x])*20.02002):int(start[x+1]*20.02002), :]
+    return scene, start
